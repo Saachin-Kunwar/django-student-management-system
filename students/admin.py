b73be7f424db715160139c17daa5c3244fa633f7
Sachin <kunwarsachin003@gmail.com>
2026-08-15 12:18:30 +0545

    feat: configure Student model in Django Admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','course','created_at')
+    search_fields= ('name', 'email','course')
+    list_filter=('course','created_at')
