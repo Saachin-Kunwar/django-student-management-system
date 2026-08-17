964cc09d1e5c2f511d64b53ec50e0bedc5be3150
Sachin <kunwarsachin003@gmail.com>
2026-08-17 20:43:13 +0545

    feat: modify ui and added department and club

--- students/views.py
+++ students/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationStyleForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from .models import Student
 from .forms import StudentForm
-
 # --- AUTHENTICATION VIEWS ---
 
 def register_view(request):
